79d84967d4829df5a31cd6e97bcdcbe90774b307
yangli-stu <yangli@weitu.ai>
2026-01-16 14:12:35 +0800

    add log.

--- src/main/java/io/stu/notify/stomp/StompWebsocketInterceptor.java
+++ src/main/java/io/stu/notify/stomp/StompWebsocketInterceptor.java
@@ -9,6 +9,7 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.simp.user.SimpSession;
 import org.springframework.messaging.simp.user.SimpUser;
 import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -16,6 +17,7 @@ import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Component;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -43,15 +45,18 @@ public class StompWebsocketInterceptor implements ChannelInterceptor {
                     log.error("token is overtime");
                     throw new IllegalStateException("The token is illegal");
                 }
-                if (Optional.ofNullable(simpUserRegistry.getUser(userId)).map(SimpUser::getSessions).map(Set::size).orElse(0) > 10) {
-                    log.error("异常 user {} connect stomp exceed max limit", userId);
+                List<String> existingSessions = Optional.ofNullable(simpUserRegistry.getUser(userId))
+                    .map(SimpUser::getSessions)
+                    .map(sessions -> sessions.stream().map(SimpSession::getId).toList())
+                    .orElse(List.of());
+                if (existingSessions.size() > 10) {
+                    log.error("user {} connect stomp exceed max limit", userId);
                     throw new IllegalStateException("The user connect stomp exceed max limit");
                 }
-                // 注册用户
                 accessor.setUser(new MyPrincipal(userId));
-                log.info("【{}】用户上线了, 当前总用户数 {}", userId, simpUserRegistry.getUserCount());
+                log.info("【{}】用户上线了, 当前新会话：{}, 当前总用户数 {}，已存在会话：{}", userId, accessor.getSessionId(), simpUserRegistry.getUserCount(), existingSessions);
             } else if(StompCommand.DISCONNECT.equals(accessor.getCommand())){
-                log.info("【{}】用户下线了, 当前总用户数 {}",accessor.getUser().getName(), simpUserRegistry.getUserCount());
+                log.info("【{}】用户下线了, 当前会话：{}, 当前总用户数 {}", Optional.ofNullable(accessor.getUser()).map(Principal::getName).orElse(null), accessor.getSessionId(), simpUserRegistry.getUserCount());
             }
         }
         return message;
